afc46ae46aeeba784a0b1a21eb897af5c29e378b
EduardoHenSil <eduardo.hen90@gmail.com>
2023-02-07 10:26:13 -0300

    Improved Syn scanner

--- scapy_syn_scanner.py
+++ scapy_syn_scanner.py
@@ -8,17 +8,19 @@ def icmp_probe(ip):
     return resp_packet is not None
 
 
-def syn_scan(ip, port):
+def syn_scan(ip, port, verbose=False):
     syn_packet = IP(dst=ip)/TCP(dport=port, flags='S')
     resp_packet = sr1(syn_packet)
-    print(resp_packet.show())
-    print('CHECK: ', resp_packet['TCP'].flags == 'SA')
+    if verbose:
+        print(resp_packet.show())
+    return resp_packet['TCP'].flags == 'SA'  # == 0x12 (0x02 == SYN + 0x10 == ACK)
 
 
 if __name__ == '__main__':
     ip = sys.argv[1]
     port = int(sys.argv[2])
     if icmp_probe(ip):
-        syn_scan(ip, port)
+        if syn_scan(ip, port):
+            print(f'{port} => open')
     else:
         print(f'PING Failed!')
